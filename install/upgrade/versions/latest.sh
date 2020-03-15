2b3e29fd7a69f1dcd3e5e65a90d6fb409155eb2e
Raphael Schneeberger <rs@scit.ch>
2020-03-15 22:09:43 +0100

    Add Z-Push during upgrade existing hestia systems.

--- install/upgrade/versions/latest.sh
+++ install/upgrade/versions/latest.sh
@@ -11,3 +11,10 @@ if [ -e "/etc/exim4/exim4.conf.template" ]; then
     echo "(*) Updating exim4 configuration..."
     sed -i "s|rfc1413_query_timeout = 5s|rfc1413_query_timeout = 0s|g" /etc/exim4/exim4.conf.template
 fi
+
+# Add Z-Push ActiveSync/AutoDiscover to mail stack
+if [ -z "$MAIL_SYSTEM" ] && [ -z "$IMAP_SYSTEM" ]; then
+    echo "(*) Configuring Z-Push ActiveSync & AutoDiscover service..."
+    apt-get -qq update && apt-get install hestia-zpush
+    cp -f $HESTIA/install/deb/zpush/zpush_params /etc/nginx/conf.d/
+fi
